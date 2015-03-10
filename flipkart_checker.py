fcf1fa5927a918d2370ea86a0c05e679c8008840
prakash <prakash.gbpec@gmail.com>
2015-03-11 01:56:45 +0530

    url of product added

--- flipkart_checker.py
+++ flipkart_checker.py
@@ -2,7 +2,7 @@ import urllib2
 import re
 import subprocess
 # URL to the product
-flipkart_url = 'URL to product'
+flipkart_url = 'http://www.flipkart.com/cooler-master-notepal-l1-cooling-pad/p/itmdyh7fzpggufdc?pid=ACCDYH7DHYZYDQMH&srno=b_1&offer=DOTDOnCoolingPads_Mar10.&ref=f3f70743-9f5c-4c8c-afe4-42a9d57d0274'
 flipkart_fetch = urllib2.urlopen(flipkart_url).read()
 # the magic line (change this accordingly :) )
 product = re.findall('Notify me when this product is in stock', flipkart_fetch);
